ae356c0b353ef338f85e61e42ed2cb9bc61ff168
AndyHoette <a.h.hoette@wustl.edu>
2024-11-01 16:47:16 -0500

    Changed starter code to have users socket id to print as if it was their user name

--- filesToServe/clientSide.js
+++ filesToServe/clientSide.js
@@ -1,11 +1,15 @@
 let socketio = io.connect();
+socketio.on("connect", function(socket){
+    localStorage.setItem("socketId", socketio.id);
+});
 socketio.on("message_to_client",function(data) {
     //Append an HR thematic break and the escaped HTML of the new message
     document.getElementById("chatlog").appendChild(document.createElement("hr"));
-    document.getElementById("chatlog").appendChild(document.createTextNode(data['message']));
+    document.getElementById("chatlog").appendChild(document.createTextNode(data["username"] + ": " + data['message']));
 });
 
 function sendMessage(){
     let msg = document.getElementById("message_input").value;
-    socketio.emit("message_to_server", {message:msg});
+    let mySocketId = localStorage.getItem("socketId");
+    socketio.emit("message_to_server", {message:msg, id:mySocketId});
 }
--- serverSide.js
+++ serverSide.js
@@ -1,20 +1,50 @@
 // Require the packages we will use:
 const http = require("http"),
-    fs = require("fs");
+	fs = require("fs"),
+	url = require("url"),
+	mime = require("mime"),
+	path = require("path");
 
 const port = 3456;
 const file = "filesToServe/index.html";
 // Listen for HTTP connections.  This is essentially a miniature static file server that only serves our one file, client.html, on port 3456:
-const server = http.createServer(function (req, res) {
-    // This callback runs when a new connection is made to our HTTP server.
-
-    fs.readFile(file, function (err, data) {
-        // This callback runs when the client.html file has been read from the filesystem.
-
-        if (err) return res.writeHead(500);
-        res.writeHead(200);
-        res.end(data);
-    });
+const server = http.createServer(function (req, resp) {
+let filename = path.join(__dirname, "filesToServe", url.parse(req.url).pathname);
+	if(url.parse(req.url).pathname=="/"){
+		filename = path.join(__dirname, "filesToServe", "/index.html");
+	}
+	(fs.exists || path.exists)(filename, function(exists){
+		if (exists) {
+			fs.readFile(filename, function(err, data){
+				if (err) {
+					// File exists but is not readable (permissions issue?)
+					resp.writeHead(500, {
+						"Content-Type": "text/plain"
+					});
+					resp.write("Internal server error: could not read file");
+					resp.end();
+					return;
+				}
+				
+				// File exists and is readable
+				var mimetype = mime.getType(filename);
+				resp.writeHead(200, {
+					"Content-Type": mimetype
+				});
+				resp.write(data);
+				resp.end();
+				return;
+			});
+		}else{
+			// File does not exist
+			resp.writeHead(404, {
+				"Content-Type": "text/plain"
+			});
+			resp.write("Requested file not found: "+filename);
+			resp.end();
+			return;
+		}
+	});
 });
 server.listen(port);
 
@@ -31,7 +61,8 @@ io.sockets.on("connection", function (socket) {
     socket.on('message_to_server', function (data) {
         // This callback runs when the server receives a new message from the client.
 
-        console.log("message: " + data["message"]); // log it to the Node.JS output
-        io.sockets.emit("message_to_client", { message: data["message"] }) // broadcast the message to other users
+        console.log("message: " + data["message"]); // log it to the Node.JS outpu:
+	console.log("socketId of user who sent that" + data["id"]);
+        io.sockets.emit("message_to_client", { message: data["message"], username: data["id"] }) // broadcast the message to other users
     });
 });
