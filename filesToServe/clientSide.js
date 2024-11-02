b3e39fa082451bb8014dcb8582e921b14cf7a948
AndyHoette <a.h.hoette@wustl.edu>
2024-11-01 22:55:55 -0500

    Layed out More of the format and css

--- filesToServe/clientSide.js
+++ filesToServe/clientSide.js
@@ -1,15 +1,21 @@
 let socketio = io.connect();
 socketio.on("connect", function(socket){
-    localStorage.setItem("socketId", socketio.id);
+	localStorage.setItem("socketId", socketio.id);
 });
 socketio.on("message_to_client",function(data) {
-    //Append an HR thematic break and the escaped HTML of the new message
-    document.getElementById("chatlog").appendChild(document.createElement("hr"));
-    document.getElementById("chatlog").appendChild(document.createTextNode(data["username"] + ": " + data['message']));
+	//Append an HR thematic break and the escaped HTML of the new message
+	document.getElementById("chatlog").appendChild(document.createElement("hr"));
+	let newMessage = document.createElement("p");
+	newMessage.appendChild(document.createTextNode(data["username"] + ": " + data["message"]));
+	document.getElementById("chatlog").appendChild(newMessage);
 });
 
 function sendMessage(){
-    let msg = document.getElementById("message_input").value;
-    let mySocketId = localStorage.getItem("socketId");
-    socketio.emit("message_to_server", {message:msg, id:mySocketId});
+	let msg = document.getElementById("message_input").value;
+	if(msg === ""){
+		return;
+	}
+	let mySocketId = localStorage.getItem("socketId");
+	document.getElementById("message_input").value = "";
+	socketio.emit("message_to_server", {message:msg, id:mySocketId});
 }
