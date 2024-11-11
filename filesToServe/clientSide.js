42e06ddd4f0dcc59ba4d57ff32da2817aa5788aa
Joshua Lee <joshlee@ip-172-31-94-126.ec2.internal>
2024-11-10 21:18:35 -0600

    comments

--- filesToServe/clientSide.js
+++ filesToServe/clientSide.js
@@ -17,16 +17,19 @@ let banButton = document.getElementById("banButton");
 let adminButton = document.getElementById("adminButton");
 let dmButton = document.getElementById("dmButton");
 
+
 function clearChat(){
 	chatLog.innerHTML = "";
 }
 
+//Session Function 
 function newSession(){
 	sessionStorage.clear();
 	sessionStorage.setItem("room", 0);
 	loggedOut();
 }
 
+//Function managing kicks
 function kicked(){
 	socketio.emit("requestToJoinRoom", {roomNumber: "0", password: "", userId: socketio.id, oldRoomNumber: sessionStorage.getItem("room")});
 	listOfUsers.innerHTML = "";
@@ -35,18 +38,18 @@ function kicked(){
 let socketio = io.connect();
 socketio.on("connect", function(socket){
 	newSession();
-	console.log("requesting all rooms");
+	//console.log("requesting all rooms");
 	socketio.emit("listRooms", {});
 });
 
 socketio.on("roomListed", function(data){
-	console.log(data);
+	//console.log(data);
 });
 
 socketio.on("message_to_client",function(data) {
 	//Append an HR thematic break and the escaped HTML of the new message
-	console.log("recieved a message");
-	console.log(data);
+	//console.log("recieved a message");
+	//console.log(data);
 	if(!data["success"]){
 		return;
 	}
@@ -74,6 +77,7 @@ socketio.on("usernameRequestReturn", function(data) {
 });
 
 
+//Function that changes the Roomname whenever a user creates a new room
 function updateRoomName(newName){
 	document.getElementById("roomName").innerHTML = "Welcome to " + newName + "!";
 }
@@ -90,7 +94,7 @@ socketio.on("joinedRoom", function(data){
 });
 
 socketio.on("usersChanged", function(data){
-	console.log(data);
+	//console.log(data);
 });
 
 socketio.on("kicked", function(data){
@@ -102,7 +106,7 @@ socketio.on("directMessage", function(data){
 });
 
 socketio.on("roomCreated", function(data) {
-	console.log("Create room with name: " + data["roomName"] + ". Needs Password? " + data["hasPassword"]);
+	//console.log("Create room with name: " + data["roomName"] + ". Needs Password? " + data["hasPassword"]);
 	let newRoomListing = document.createElement("div");
 	newRoomListing.classList.add("roomListing");
 	let newRoomListingTitle = document.createElement("p");
@@ -130,7 +134,7 @@ socketio.on("clearUsers", function(data){
 });
 
 socketio.on("addUser", function(data){
-	console.log(data);
+	//console.log(data);
 	let myButton = document.createElement("button");
 	myButton.innerHTML = data["username"];
 	myButton.value = data["userId"];
@@ -142,6 +146,7 @@ socketio.on("forceJoin", function(data){
 	socketio.emit("requestToJoinRoom", {roomNumber: data["roomNumber"], password: "", userId: socketio.id, oldRoomNumber: sessionStorage.getItem("room")});
 });
 
+//Create a function for all the actions to work
 function setUpAction(buttonThatWasPressed){
 	actionBar.style.display="block";
 	document.getElementById("affectedUser").innerHTML = buttonThatWasPressed.innerHTML;
@@ -152,6 +157,7 @@ function setUpAction(buttonThatWasPressed){
 	dmButton.value = idAffected;
 }
 
+//Get the messenge sent by user and sends the message in the room
 function sendMessage(){
 	let msg = document.getElementById("message_input").value;
 	if(msg === ""){
@@ -161,6 +167,7 @@ function sendMessage(){
 	socketio.emit("message_to_server", {message:msg, id:socketio.id, roomNumber: sessionStorage.getItem("room")});
 }
 
+
 function requestUsername(){
 	let newUsername = document.getElementById("usernameInput").value;
 	if(newUsername === ""){
@@ -169,6 +176,7 @@ function requestUsername(){
 	socketio.emit("usernameRequest", {username: newUsername, id:socketio.id});
 }
 
+// Login Function
 function loggedIn(){
 	document.getElementById("beforeLogIn").style.display = "none";
 	document.getElementById("afterLogIn").style.display = "block";
@@ -176,7 +184,7 @@ function loggedIn(){
 	document.getElementById("createRoom").style.display = "block";
 	socketio.emit("requestToJoinRoom", {roomNumber: 0, password: "", userId: socketio.id, oldRoomNumber: sessionStorage.getItem("room")});
 }
-
+// Logout Function
 function loggedOut(){
 	document.getElementById("beforeLogIn").style.display = "block";
 	document.getElementById("afterLogIn").style.display = "none";
@@ -189,12 +197,13 @@ function loggedOut(){
 	clearChat();
 	kicked();
 }
-
+// Choosing PFP Function
 function updatePFP(){
 	socketio.emit("updatePFP", {id:socketio.id, pfp: pfpSelector.value});
 	userPFP.src = "profilePictures/" + pfpSelector.value + ".png";
 }
 
+//Create the rooms with optional password
 function createRoom(){
 	console.log("Creating Room ");
 	console.log(newRoomName.value);
@@ -203,30 +212,32 @@ function createRoom(){
 	newRoomName.value = "";
 }
 
+//Join a room that was clicked and if a password is needed, you must enter the right password to join
 function joinRoom(buttonThatWasClicked){
-	console.log(buttonThatWasClicked.value);
+	//console.log(buttonThatWasClicked.value);
 	passwordAttempt = buttonThatWasClicked.parentNode.childNodes[1].value;
-	console.log(passwordAttempt);
+	//console.log(passwordAttempt);
 	socketio.emit("requestToJoinRoom", {roomNumber: buttonThatWasClicked.value, password: passwordAttempt, userId: socketio.id, oldRoomNumber: sessionStorage.getItem("room")});
 }
 
-
+//Function to kick from room
 function requestKick(buttonThatWasClicked){
 	userToKick = buttonThatWasClicked.value;
 	socketio.emit("requestToKickUser", {"user": userToKick, "room":sessionStorage.getItem("room")});
 }
 
+//Function to ban from room
 function requestBan(buttonThatWasClicked){
 	userToBan = buttonThatWasClicked.value;
 	socketio.emit("requestToBanUser", {"user": userToBan, "room":sessionStorage.getItem("room")});
 }
-
+//Function to give admin to user
 function requestAdmin(buttonThatWasClicked){
 	userToAdmin = buttonThatWasClicked.value;
-	console.log("requestingAdmin");
+	//console.log("requestingAdmin");
 	socketio.emit("requestToAdminUser", {"user": userToAdmin, "room":sessionStorage.getItem("room")});
 }
-
+//Function to DM someone who is in the same room
 function requestDM(buttonThatWasClicked){
 	userToDM = buttonThatWasClicked.value;
 	socketio.emit("requestToDM", {"user": userToDM, "room":sessionStorage.getItem("room")});
